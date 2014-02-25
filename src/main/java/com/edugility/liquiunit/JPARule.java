ec49a9880e1f1339ad2243e590ca4b1061e68084
Laird Nelson <ljnelson@gmail.com>
2014-02-25 10:57:45 -0800

    Added documentation

--- src/main/java/com/edugility/liquiunit/JPARule.java
+++ src/main/java/com/edugility/liquiunit/JPARule.java
@@ -31,6 +31,7 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -46,32 +47,156 @@ import org.junit.runner.Description;
 
 import org.junit.runners.model.Statement;
 
+/**
+ * An {@link ExternalResource} that sets up an {@link EntityManager}
+ * before each JUnit test run.
+ *
+ * <p>Clients may choose whether a single {@link EntityManagerFactory}
+ * is used to create all {@link EntityManager}s, or whether an {@link
+ * EntityManagerFactory} is created and {@linkplain
+ * EntityManagerFactory#close() closed} before and after each test
+ * run.</p>
+ *
+ * <p>Although creating an {@link EntityManagerFactory} can be a
+ * heavyweight operation, it offers much more isolation than the
+ * alternative, and the intensity of its resource usage can be offset
+ * by running tests in parallel.</p>
+ *
+ * @author <a href="http://about.me/lairdnelson"
+ * target="_parent">Laird Nelson</a>
+ *
+ * @see EntityManagerFactory
+ *
+ * @see ExternalResource
+ *
+ * @see Persistence#createEntityManagerFactory(String)
+ */
 public class JPARule extends ExternalResource {
 
+  /**
+   * The instance of the test that this {@link JPARule} is decorating.
+   *
+   * <p>This field may be {@code null}.</p>
+   */
   private final Object testInstance;
 
-  private List<Field> fields;
+  /**
+   * A {@link Collection} of {@link Field}s that house {@link
+   * EntityManager}s; used by the {@link #before()} method for
+   * implementing injection.
+   *
+   * <p>This field may be {@code null}.</p>
+   *
+   * @see #before()
+   */
+  private Collection<Field> fields;
 
+  /**
+   * A {@link Description} describing the current test.
+   *
+   * <p>This field may be {@code null}.</p>
+   */
   private Description description;
 
+  /**
+   * The {@link EntityManager} in effect for the current test.
+   *
+   * <p>This field may be {@code null}.</p>
+   */
   private EntityManager em;
 
+  /**
+   * The {@link EntityManagerFactory} in effect for the current test.
+   *
+   * <p>This field may be {@code null}.</p>
+   */
   private EntityManagerFactory emf;
 
+  /**
+   * Whether or not the {@link EntityManagerFactory} should be
+   * {@linkplain EntityManagerFactory#close() closed} in the {@link
+   * #after()} method.  Typically this field is set to {@code true}
+   * when {@linkplain #JPARule(Object, EntityManagerFactory) an
+   * <code>EntityManagerFactory</code> is passed in at construction
+   * time}.
+   *
+   * @see #after()
+   *
+   * @see #JPARule(Object, EntityManagerFactory)
+   */
   private boolean closeFactory;
 
+  /**
+   * The name of the persistence unit for which an {@link
+   * EntityManagerFactory} should be created.
+   *
+   * <p>This field may be {@code null}.</p>
+   */
   private final String persistenceUnitName;
 
+  /**
+   * A {@link Map} representing properties to be used during
+   * {@linkplain Persistence#createEntityManagerFactory(String, Map)
+   * <code>EntityManagerFactory</code> creation}.
+   *
+   * <p>This field may be {@code null}.</p>
+   */
   private final Map<?, ?> entityManagerFactoryProperties;
 
+  /**
+   * A {@link Map} representing properties to be used during
+   * {@linkplain EntityManagerFactory#createEntityManager(Map)
+   * <code>EntityManager</code> creation}.
+   *
+   * <p>This field may be {@code null}.</p>
+   */
   private final Map<?, ?> entityManagerProperties;
 
+  /**
+   * A {@link H2Rule} that might have been passed in at {@linkplain
+   * #JPARule(Object, String, H2Rule) construction time}.  This {@link
+   * H2Rule} will be used in that case to {@linkplain
+   * H2Rule#getJPAProperties() produce properties} for a new {@link
+   * EntityManagerFactory}.
+   *
+   * <p>This field may be {@code null}.</p>
+   *
+   * @see #JPARule(Object, String, H2Rule)
+   *
+   * @see H2Rule#getJPAProperties()
+   */
   private final H2Rule h2Rule;
 
+
+  /*
+   * Constructors.
+   */
+
+
+  /**
+   * Creates a new {@link JPARule}.
+   *
+   * @param testInstance an instance of the test class; must not be
+   * {@code null}
+   *
+   * @param persistenceUnitName the name of the persistence unit to
+   * look for; must not be {@code null}
+   *
+   * @param h2Rule an {@link H2Rule} whose {@link
+   * H2Rule#getJPAProperties()} method will provide the connection and
+   * authentication information; must not be {@code null}
+   *
+   * @exception IllegalArgumentException if {@code testInstance} or
+   * {@code persistenceUnitName} or {@code h2Rule} is {@code null}
+   */
   public JPARule(final Object testInstance, final String persistenceUnitName, final H2Rule h2Rule) {
     super();
     if (testInstance == null) {
       throw new IllegalArgumentException("testInstance", new NullPointerException("testInstance"));
+    } else if (persistenceUnitName == null) {
+      throw new IllegalArgumentException("persistenceUnitName", new NullPointerException("persistenceUnitName"));
+    } else if (h2Rule == null) {
+      throw new IllegalArgumentException("h2Rule", new NullPointerException("h2Rule"));
     }
     this.testInstance = testInstance;
     this.persistenceUnitName = persistenceUnitName;
@@ -80,14 +205,62 @@ public class JPARule extends ExternalResource {
     this.entityManagerProperties = null;
   }
 
+  /**
+   * Creates a new {@link JPARule}.
+   *
+   * <p>This constructor calls the {@link #JPARule(Object,
+   * EntityManagerFactory, Map)} constructor supplying it with the
+   * value of the {@code testInstance} parameter, the return value
+   * that results from invoking the {@link
+   * Persistence#createEntityManagerFactory(String)} method with the
+   * supplied {@code persistenceUnitName} parameter and {@code
+   * null}.</p>
+   * 
+   * @param testInstance an instance of the test class; must not be
+   * {@code null}
+   *
+   * @param persistenceUnitName the name of the persistence unit to
+   * look for; must not be {@code null}
+   *
+   * @exception IllegalArgumentException if {@code testInstance} or
+   * {@code persistenceUnitName} is {@code null}
+   *
+   * @exception NullPointerException if {@code persistenceUnitName} is
+   * {@code null}
+   *
+   * @see #JPARule(Object, EntityManagerFactory, Map)
+   */
   public JPARule(final Object testInstance, final String persistenceUnitName) {
     this(testInstance, Persistence.createEntityManagerFactory(persistenceUnitName), null);
   }
 
+  /**
+   * Creates a new {@link JPARule}.
+   *
+   * @param testInstance an instance of the test class; must not be
+   * {@code null}
+   *
+   * @param persistenceUnitName the name of the persistence unit to
+   * look for; must not be {@code null}
+   *
+   * @param entityManagerFactoryProperties a {@link Map} of properties
+   * to use when {@linkplain
+   * Persistence#createEntityManagerFactory(String, Map) creating a
+   * new <code>EntityManagerFactory</code>}; may be {@code null}
+   *
+   * @param entityManagerProperties a {@link Map} of properties to use
+   * when {@linkplain EntityManagerFactory#createEntityManager(Map)
+   * creating a new <code>EntityManager</code>}; may be {@code null}
+   *
+   * @exception IllegalArgumentException if {@code testInstance} or
+   * {@code persistenceUnitName} is {@code null}
+   */
   public JPARule(final Object testInstance, final String persistenceUnitName, final Map<?, ?> entityManagerFactoryProperties, final Map<?, ?> entityManagerProperties) {
     super();
     if (testInstance == null) {
       throw new IllegalArgumentException("testInstance", new NullPointerException("testInstance"));
+    } else if (persistenceUnitName == null) {
+      throw new IllegalArgumentException("persistenceUnitName", new NullPointerException("persistenceUnitName"));
     }
     this.testInstance = testInstance;
     this.persistenceUnitName = persistenceUnitName;
@@ -96,10 +269,44 @@ public class JPARule extends ExternalResource {
     this.entityManagerProperties = entityManagerProperties;
   }
 
+  /**
+   * Creates a new {@link JPARule}.
+   *
+   * <p>This constructor calls the {@link #JPARule(Object,
+   * EntityManagerFactory, Map)} constructor, supplying {@code null}
+   * for the third parameter.</p>
+   *
+   * @param testInstance an instance of the test class; must not be
+   * {@code null}
+   *
+   * @param emf an {@link EntityManagerFactory}; may be {@code null}
+   *
+   * @exception IllegalArgumentException if {@code testInstance} is
+   * {@code null}
+   *
+   * @see #JPARule(Object, EntityManagerFactory, Map)
+   */
   public JPARule(final Object testInstance, final EntityManagerFactory emf) {
     this(testInstance, emf, null);
   }
 
+  /**
+   * Creates a new {@link JPARule}.
+   *
+   * @param testInstance an instance of the test class; must not be
+   * {@code null}
+   *
+   * @param emf the {@link EntityManagerFactory} to use; may be {@code
+   * null}
+   *
+   * @param entityManagerProperties the {@link Map} of properties to
+   * use when {@linkplain
+   * EntityManagerFactory#createEntityManager(Map) creating a new
+   * <code>EntityManager</code>}; may be {@code null}
+   *
+   * @exception IllegalArgumentException if {@code testInstance} is
+   * {@code null}
+   */
   public JPARule(final Object testInstance, final EntityManagerFactory emf, final Map<?, ?> entityManagerProperties) {
     super();
     if (testInstance == null) {
@@ -113,8 +320,29 @@ public class JPARule extends ExternalResource {
     this.emf = emf;
   }
 
+
+  /*
+   * Instance methods.
+   */
+
+
+  /**
+   * Creates an {@link EntityManager} and injects it into all fields
+   * in the test instance that are of type {@code EntityManager} and
+   * that are annotated with {@link
+   * PersistenceContext @PersistenceContext}.
+   *
+   * @exception IllegalAccessException if there was a reflection
+   * problem
+   *
+   * @exception InvocationTargetException if there was a reflection
+   * problem
+   */
   @Override
   public void before() throws IllegalAccessException, InvocationTargetException {
+
+    // If there's no EntityManagerFactory yet, or a pre-existing one,
+    // acquire a new one for this test.
     if (this.emf == null || !this.emf.isOpen()) {
       this.closeFactory = true;
       final Map<?, ?> entityManagerFactoryProperties = this.getEntityManagerFactoryProperties();
@@ -124,9 +352,16 @@ public class JPARule extends ExternalResource {
         this.emf = Persistence.createEntityManagerFactory(this.persistenceUnitName, entityManagerFactoryProperties);
       }
     }
+
     if (this.emf != null && this.emf.isOpen()) {
-      final Thread currentThread = Thread.currentThread();
-      assert currentThread != null;
+
+      // If for some crazy reason there's an open EntityManager
+      // already installed, close it.
+      if (this.em != null && this.em.isOpen()) {
+        this.em.close();
+      }
+
+      // Create a new EntityManager.
       if (this.entityManagerProperties == null || this.entityManagerProperties.isEmpty()) {
         this.em = this.emf.createEntityManager();
       } else {
@@ -136,7 +371,14 @@ public class JPARule extends ExternalResource {
       if (this.description != null) {
         final Class<?> testClass = description.getTestClass();
         if (testClass != null) {
-          final List<Field> fields = new ArrayList<Field>();
+
+          // Look for all @PersistenceContext-annotated fields in the
+          // test class and its superclasses.  Set the just-created
+          // EntityManager into all of them on the current test
+          // instance.  Keep track of which fields were set so they
+          // can be cleared in the after() method.
+
+          final Collection<Field> fields = new ArrayList<Field>();
           Class<?> cls = testClass;
           while (cls != null) {
             final Field[] declaredFields = cls.getDeclaredFields();
@@ -169,6 +411,25 @@ public class JPARule extends ExternalResource {
     }
   }
 
+  /**
+   * Returns a {@link Map} that will be passed to the {@link
+   * Persistence#createEntityManagerFactory(String, Map)} method to
+   * create a new {@link EntityManagerFactory}.
+   *
+   * <p>Note that if an {@link H2Rule} has been passed to the
+   * {@linkplain #JPARule(Object, String, H2Rule) relevant
+   * constructor}, then the {@link Map} returned will be the return
+   * value of the {@link H2Rule#getJPAProperties()} method.</p>
+   *
+   * <p>This method may return {@code null}.</p>
+   *
+   * @return a {@link Map} of {@link EntityManagerFactory} properties,
+   * or {@code null}
+   *
+   * @see Persistence#createEntityManagerFactory(String, Map)
+   * 
+   * @see H2Rule#getJPAProperties()
+   */
   public Map<?, ?> getEntityManagerFactoryProperties() {
     final Map<?, ?> returnValue;
     if (this.h2Rule != null) {
@@ -209,6 +470,17 @@ public class JPARule extends ExternalResource {
     return super.apply(base, description);
   }
 
+  /**
+   * {@linkplain EntityTransaction#rollback() Rolls back} any
+   * {@linkplain EntityTransaction#isActive() active} {@link
+   * EntityTransaction}, {@linkplain EntityManager#close() closes} any
+   * {@linkplain EntityManager#isOpen() open} {@link EntityManager},
+   * sets all fields that were injected by the {@link #before()}
+   * method to {@code null}, and closes any {@link
+   * EntityManagerFactory} that was created by this {@link JPARule}.
+   *
+   * @see #before()
+   */
   @Override
   public void after() {
     if (this.em != null && this.em.isOpen()) {
@@ -242,20 +514,42 @@ public class JPARule extends ExternalResource {
     this.description = null;
   }
 
+  /**
+   * Forcibly closes any open {@link EntityManager}s and {@link
+   * EntityManagerFactory} instances, thus releasing resources.
+   *
+   * <p>Most callers do not need to invoke this method.</p>
+   */
   public void close() {
-    if (this.em != null && this.em.isOpen()) {
+    if (this.em != null) {
       this.em.close();
       this.em = null;
     }
     if (this.emf != null) {
       this.emf.close();
+      this.emf = null;
     }
   }
 
+  /**
+   * Returns the {@link EntityManagerFactory} in effect.
+   *
+   * <p>This method may return {@code null}.</p>
+   *
+   * @return the {@link EntityManagerFactory} in effect, or {@code
+   * null}
+   */
   public EntityManagerFactory getEntityManagerFactory() {
     return this.emf;
   }
 
+  /**
+   * Returns the {@link EntityManager} in effect.
+   *
+   * <p>This method may return {@code null}.</p>
+   *
+   * @return the {@link EntityManager} in effect, or {@code null}
+   */
   public EntityManager getEntityManager() {
     return this.em;
   }
